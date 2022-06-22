535198abd65be100f2a89c352ef00ef3336a2fa3
Miacarmen <maitreyacarmen@gmail.com>
2022-06-21 17:25:58 -0700

    nav and footer components added, base products pages, router and routes added to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
-import './App.css';
-import Home from './pages/Home';
-import Nav from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Products from './pages/Products';
+import ViewProduct from './pages/ViewProduct';
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="">
-    
-      <Nav />
-      <Home />
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column min-100-vh">
+          <Nav />
+          <div className="">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:productId" element={<ViewProduct />} />
+            </Routes>
+          
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
